ebf6380a530fed4f13279ce5983c6c1eefb01817
NewUser <newuser@example.com>
2014-07-08 20:55:59 +0530

    completed rspec controller and model testing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
       @product.cost = params[:cost]
        if @product.save
           flash[:notce] ="product successfully create"
-          redirect_to @product
+          render :json => "successfully create product"
        else
           flash[:error] = "product not create"
           render :action => 'new'
@@ -21,11 +21,34 @@ class ProductsController < ApplicationController
  
   def show
     @product = Product.find(params[:id])
-    # puts "/////////////////////////////////////////////////"
-    render :json => @product.inspect and return
+    render :json => "successfully show product"
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    render :json => "successfully show product"
+  end
+  
+  def update
+    @product = Product.find(params[:id])
+    #puts params.inspect
+    @product.name = params[:product][:name]
+    @product.cost = params[:product][:cost]
+    @product.save
+    render :json => "successfully updated product"
+  end
 
+  def destroy
+   @product = Product.find(params[:id])
+   Product.destroy(@product.id)
+   flash[:notice] = "deleted product successfully"
+   #render :json => "successfully deleted product"
+    redirect_to {action => "index"}
+   
+  end
 
+  def index
+    render :json => "dflkflsdf"
+  end
 
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, :type => :controller do
 
-describe "POST create" do
+describe "post create" do
   describe "with valid params" do
     it "creates a new product" do
      request.accept = "application/json"  
@@ -16,15 +16,54 @@ describe "POST create" do
 end
 
 describe "get show" do
-  describe "with valid params" do
-    it "creates a new product" do
-     request.accept = "application/json"  
-        post :create,{:name =>"apple",:cost =>12548}
+  describe "to show particular product" do
+    it "show the created product" do
+     request.accept = "application/json"
+      @product = FactoryGirl.create(:product)  
+        get :show,:id => @product.id
         expect(response.status).to eq(200)    
     end
   end
 end
 
+describe "get edit" do
+  describe "to edit particular product" do
+    it "edit product action" do
+     request.accept = "application/json"
+      @product = FactoryGirl.create(:product)  
+        get :edit,:id => @product.id
+        expect(response.status).to eq(200)    
+    end
+  end
+end
 
+ describe "put update" do
+   describe "to update particular one" do
+     it "update product action" do
+      request.accept = "application/json"
+      @product = FactoryGirl.create(:product) 
+      puts(@product.name)
+      @product.name = 'update product'
+      put :update,:id => @product.id, :product=>{:name=> @product.name, :cost=>@product.cost}
+      expect(response.status).to eq(200)
+      @product.reload
+      expect(@product.name).to eq('update product')
+      #response.should redirect_to(products_url)
+     end
+   end
+ end
+
+ describe "delete destroy " do
+   describe "with valid params" do
+     it "delele paticulat product" do
+      request.accept = "application/json"
+      @product = FactoryGirl.create(:product)
+       @products = Product.count
+       delete :destroy,:id => @product.id
+       expect(response.status).to eq(200) 
+       expect(@products-1).to eq(Product.count)   
+     end
+   end
+ end
 
 end
